468ab36d0e5046c65d8bfa8153626cf0f065c865
Brennan Grieve <46986566+brennanGrieve@users.noreply.github.com>
2021-01-16 16:50:35 +1300

    CSS + User feedback Implementation for the contact us form.
    Also includes checking for empty/null information and prompting the user to retry submission.

--- E-Com-Website/src/app/contact-page/contact-page.component.ts
+++ E-Com-Website/src/app/contact-page/contact-page.component.ts
@@ -10,6 +10,11 @@ import { StoreDataClientService } from '../store-data-client-service.service';
 })
 export class ContactPageComponent implements OnInit {
   contactForm;  
+  nameFailed;
+  mailFailed;
+  queryFailed;
+  sendSuccess;
+
   constructor(
     private builder: FormBuilder,
     private client: StoreDataClientService
@@ -28,7 +33,23 @@ export class ContactPageComponent implements OnInit {
     * Once the backend is ready, use angular's HTTP module to POST to server. Perhaps encapsulate in another service that focuses
     * entirely on PUT/POST methods? Already have one for GET methods.
     */
+   this.sendSuccess = false;
+   this.checkQueryData(queryData);
+   if(this.queryFailed == true || this.mailFailed == true || this.nameFailed == true){return}
    this.client.POSTContactQuery(queryData);
+   this.contactForm.reset();
+   this.sendSuccess = true;
   }
 
+  checkQueryData(queryData){
+    if(queryData.name == '' || queryData.name == null){
+      this.nameFailed = true;
+    }else{this.nameFailed = false}
+    if(queryData.email == '' || queryData.email == null){
+      this.mailFailed = true;
+    }else{this.mailFailed = false}
+    if(queryData.query == '' || queryData.query == null){
+      this.queryFailed = true;
+    }else{this.queryFailed = false}
+  }
 }
