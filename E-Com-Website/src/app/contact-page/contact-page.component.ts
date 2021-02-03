3dda3a997d95bf601c86b4f242bfe9db2acaf20d
Brennan Grieve <46986566+brennanGrieve@users.noreply.github.com>
2021-02-03 19:44:51 +1300

    Fixes + Sign Up page basic setup + dependency injection

--- E-Com-Website/src/app/contact-page/contact-page.component.ts
+++ E-Com-Website/src/app/contact-page/contact-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { StoreDataClientService } from '../store-data-client-service.service';
 
 
