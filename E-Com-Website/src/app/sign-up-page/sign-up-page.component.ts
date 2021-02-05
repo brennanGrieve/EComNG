275f12743ecd8d83999f9a38a8856b3d3d884e18
Brennan Grieve <46986566+brennanGrieve@users.noreply.github.com>
2021-02-06 00:16:26 +1300

    Form content checking intial implementation
    Need to connect to the view.

--- E-Com-Website/src/app/sign-up-page/sign-up-page.component.ts
+++ E-Com-Website/src/app/sign-up-page/sign-up-page.component.ts
@@ -9,7 +9,15 @@ import { StoreDataClientService } from '../store-data-client-service.service'
 })
 export class SignUpPageComponent implements OnInit {
 
-  signUpForm
+  signUpForm : FormGroup;
+  uNameFailed : boolean = false;
+  passFailed : boolean = false;
+  pass2Failed : boolean = false;
+  emailFailed : boolean = false;
+  fNameFailed : boolean = false;
+  lNameFailed : boolean = false;
+  shipAddrFailed : boolean = false;
+  phNumFailed : boolean = false;
 
   constructor(
     private builder : FormBuilder,
@@ -22,8 +30,8 @@ export class SignUpPageComponent implements OnInit {
       pass : '',
       pass2 : '',
       email : '',
-      firstName : '',
-      lastName : '',
+      fName : '',
+      lName : '',
       shipAddr : '',
       phNum : '',
     })
@@ -33,8 +41,36 @@ export class SignUpPageComponent implements OnInit {
     this.checkSubmissionData(dataToSend);
   }
 
-  checkSubmissionData(toCheck){
+  /**
+   * Checks to ensure that all form fields have an acceptable value before continuing with submission to the backend.
+   * @param toCheck Submission content to be checked for nulls & Empties
+   */
 
+  checkSubmissionData(toCheck){
+      if(toCheck.uname == "" || toCheck.uname == null){
+        this.uNameFailed = true;
+      }else{this.uNameFailed = false}
+      if(toCheck.pass == "" || toCheck.pass == null){
+        this.passFailed = true;
+      }else{this.passFailed = false}
+      if(toCheck.pass2 == "" || toCheck.pass2 == null){
+        this.pass2Failed = true;
+      }else{this.pass2Failed = false}
+      if(toCheck.email == "" || toCheck.email == null){
+        this.emailFailed = true;
+      }else{this.emailFailed = false}
+      if(toCheck.fName == "" || toCheck.fName == null){
+        this.fNameFailed = true;
+      }else{this.fNameFailed = false}
+      if(toCheck.lName == "" || toCheck.lName == null){
+        this.lNameFailed = true;
+      }else{this.lNameFailed = false}
+      if(toCheck.shipAddr == "" || toCheck.shipAddr == null){
+        this.shipAddrFailed = true;
+      }else{this.shipAddrFailed = false}
+      if(toCheck.phNum == "" || toCheck.phNum == null){
+        this.phNumFailed = true
+      }else{this.phNumFailed = false}
   }
 
 }
