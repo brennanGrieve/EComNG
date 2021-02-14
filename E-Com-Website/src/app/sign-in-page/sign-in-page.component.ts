3955ccd1a074121aa4794b003c33b9136351dc68
Brennan Grieve <46986566+brennanGrieve@users.noreply.github.com>
2021-02-14 19:56:44 +1300

    Cleaning up debug statements

--- E-Com-Website/src/app/sign-in-page/sign-in-page.component.ts
+++ E-Com-Website/src/app/sign-in-page/sign-in-page.component.ts
@@ -38,7 +38,6 @@ export class SignInPageComponent implements OnInit {
      */
     if(this.checkLoginData(loginData)){
       this.auth.POSTSignInInfo(loginData).subscribe(UAuthToken =>{
-        console.log(UAuthToken);
         //put the authtoken in a cookie so that the client can use it to access the account, or if a failure is returned, handle it
         if(UAuthToken != null){
           this.cookies.addAuthCookie(UAuthToken);
